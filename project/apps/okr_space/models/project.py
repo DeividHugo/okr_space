4207203aee5457c40a7673db84fb2efda9b55301
Victor Menendes <victormenendes3101@gmail.com>
2023-06-12 17:38:09 -0300

    added steps

--- project/apps/okr_space/models/project.py
+++ project/apps/okr_space/models/project.py
@@ -1,11 +1,14 @@
 from .base import BaseModel
 from django.db import models
+from django.contrib.auth.models import User
 
 class ProjectModel(BaseModel):
     name = models.CharField(max_length=255)
+    custo = models.FloatField()
     start_date = models.DateField()
     end_date = models.DateField()
     description = models.TextField()
+    gerente = models.ForeignKey(User, on_delete=models.CASCADE, related_name='gerente')
     
     class Meta:
         db_table = 'project'
