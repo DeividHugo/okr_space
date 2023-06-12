d9c30d491eee2217870f149c35d03db6d4d8db4e
Victor Menendes <victormenendes3101@gmail.com>
2023-06-12 17:46:21 -0300

    added steps

--- project/apps/okr_space/models/project.py
+++ project/apps/okr_space/models/project.py
@@ -12,24 +12,3 @@ class ProjectModel(BaseModel):
     
     class Meta:
         db_table = 'project'
-
-class Employee(models.Model):
-    name = models.CharField(max_length=255)
-    project = models.ForeignKey(ProjectModel, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'employee'
-
-class Goal(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    description = models.TextField()
-
-    class Meta:
-        db_table = 'goal'
-
-class Bonus(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    star_count = models.IntegerField(default=0)
-
-    class Meta:
-        db_table = 'bonus'
